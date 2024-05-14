2493e3cc4ecb8003e52661e3e34af3d1e0b97f98
mjneumanabcbs <mjneumann@arkbluecross.com>
2024-05-14 14:31:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit
 import snowflake.connector
 streamlit.title('My Parents New Healthy Diner')
 
-my_cnx = snowflake.connector.connect("snowflake")
+my_cnx = snowflake.connector.connect(["snowflake"])
 my_cur = my_cnx.cursor()
